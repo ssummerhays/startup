8c535d294c864428cb7785451a71ce20d1289900
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 12:07:45 -0600

    Updated navBar and added routing for each path

--- src/app.jsx
+++ src/app.jsx
@@ -2,33 +2,47 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { NewTournament } from './newTournament/newTournament';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { AddScore } from './addScore/addScore';
+
 export default function App() {
   return (
-    <div className='body'>
-    
-    <header className="container-fluid">
-      <nav className="navbar bg-dark fixed-top navbar-dark">
-        <a className="navbar-brand" href="#">GolfLeaderboard<sup>&reg;</sup></a>
-        <menu className="navbar-nav">
-          <li className="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
-          <li className="nav-item"><a class="nav-link" href="newTournament.html">New Tournament</a></li>
-          <li className="nav-item"><a class="nav-link" href="leaderboard.html">Leaderboard</a></li>
-          <li className="nav-item"><a class="nav-link" href="addScore.html">Add Score</a></li>
-        </menu>
-      </nav>
-    </header>
+    <BrowserRouter>
+        <div className='body'>
+            <header className="container-fluid">
+                <nav className="navbar bg-dark fixed-top navbar-dark">
+                    <a className="navbar-brand" href="#">GolfLeaderboard<sup>&reg;</sup></a>
+                    <menu className="navbar-nav">
+                    <li className="nav-item"><NavLink className="nav-link" to=''>Login</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="newTournament">New Tournament</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="leaderboard">Leaderboard</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="addScore">Add Score</NavLink></li>
+                    </menu>
+                </nav>
+            </header>
 
-    <main className="container-fluid text-center">
-      App components go here
-    </main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/newTournament' element={<NewTournament />} />
+                <Route path='/leaderboard' element={<Leaderboard />} />
+                <Route path='/addScore' element={<AddScore />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-    <footer className="bg-dark">
-      <div className="container-fluid">
-        <span className="text-white-50">Spencer Summerhays</span>
-        <a className="text-white-50" href="https://github.com/ssummerhays/startup">GitHub</a>
-      </div>
-    </footer>
-
-  </div>
+            <footer className="bg-dark">
+                <div className="container-fluid">
+                    <span className="text-white-50">Spencer Summerhays</span>
+                    <a className="text-white-50" href="https://github.com/ssummerhays/startup">GitHub</a>
+                </div>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
