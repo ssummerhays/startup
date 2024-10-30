a0e68e24059df27fd8c2566133148689ce996be1
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 12:08:17 -0600

    added placeholder Leaderboard function

---
+++ src/leaderboard/leaderboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Leaderboard() {
+  return (
+    <main className='container-fluid text-center'>
+      <div>leaderboard displayed here</div>
+    </main>
+  );
+}
