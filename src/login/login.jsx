b21b343bc90c4693214bcdd99b014521c5ae618e
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 17:03:47 -0600

    converted login.html to login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
   return (
-    <main className='container-fluid text-center'>
-      <div>login displayed here</div>
+    <main className="login-main">
+        <div >
+            <h1 className='h1-title'>GolfLeaderboard</h1>
+            <form className='login-form' method="get" action="newTournament.html">
+                <div className="input-group mb-3">
+                    <input type="text" placeholder="email@example.com" className="form-control"/>
+                </div>
+                <div className="input-group mb-3">
+                    <input type="password" placeholder="password" className="form-control"/>
+                </div>
+                <div>
+                    <button className="btn btn-primary" type="submit">Login</button>
+                    <button className="btn btn-secondary" type="submit">Sign Up</button>
+                </div>
+            </form>
+        </div> 
     </main>
   );
 }
