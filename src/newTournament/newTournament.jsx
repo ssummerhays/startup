0c59a4b0651b451f01786c7d82b64919eb791903
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 17:04:16 -0600

    converted newTournament.html to newTournament.css

--- src/newTournament/newTournament.jsx
+++ src/newTournament/newTournament.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './newTournament.css'
 
 export function NewTournament() {
   return (
-    <main className='container-fluid text-center'>
-      <div>newTournament displayed here</div>
+    <main className='newTournament-main'>
+        <div className="new-tournament">
+            <h2>Create New Tournament</h2>
+            <form method="get" action="leaderboard.html">
+            <div className="input-group mb-3">
+                <input type="text" placeholder="Tournament Name" className="form-control"/>
+            </div>
+            <div className="input-group">
+                <input type="number" placeholder="Max Players" className="form-control" min="1"/>
+            </div>
+            <button className="btn btn-primary" type="submit">Create New Tournament</button>
+            </form>
+        </div>
+        <div className="join-tournament">
+            <h2>Join Tournament</h2>
+            <form method="get" action="leaderboard.html">
+            <div className="input-group">
+                <input className="form-control" type="text" placeholder="Tournament ID" />
+            </div>
+            <button className="btn btn-primary" type="submit">Join Tournament</button>
+            </form>
+        </div>
     </main>
   );
 }
