3ef65bc902cfbbb98c567a3725b9b0438a8fc750
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 12:09:05 -0600

    added placeholder NewTournament function

---
+++ src/newTournament/newTournament.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function NewTournament() {
+  return (
+    <main className='container-fluid text-center'>
+      <div>newTournament displayed here</div>
+    </main>
+  );
+}
