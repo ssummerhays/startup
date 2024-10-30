1558aa5a6b45dbd5c70e04e6b17b6c128d3ba032
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 17:02:45 -0600

    Converted addScore.html into addScore.jsx

--- src/addScore/addScore.jsx
+++ src/addScore/addScore.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './addScore.css';
 
 export function AddScore() {
   return (
-    <main className='container-fluid text-center'>
-      <div>addScore displayed here</div>
+    <main className='addScore-main'>
+      <div>
+        <h2>Add a Score</h2>
+        <form method="get" action="leaderboard.html">
+          <div>
+            <label for="hole-number">Hole#</label>
+            <input className="input-group mb-3" type="number" id="hole-number" min="1" max="18"/>
+          </div>
+          <div>
+            <label for="score">Score</label>
+            <input type="number" id="score" className="input-group mb-3" min="1" max="99"/>
+          </div>
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        <h2>Recent Scores</h2>
+        <div><span className="player-name">John</span> birdied 15</div>
+        <div><span className="player-name">James</span> parred 16</div>
+        <div><span className="player-name">Matthew</span> bogeyed 16</div>
+        <div><span className="player-name">Eli</span> birdied 15</div>
+      </div>
+
     </main>
   );
 }
