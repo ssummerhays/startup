430a6537d900701c006f3477ad965aaaf9bdb3c1
ssummerhays <spencersummerhays3@gmail.com>
2024-10-30 12:08:02 -0600

    Added placeholder AddScore function

---
+++ src/addScore/addScore.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AddScore() {
+  return (
+    <main className='container-fluid text-center'>
+      <div>addScore displayed here</div>
+    </main>
+  );
+}
