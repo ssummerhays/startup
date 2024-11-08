e056cdbe5b7be8b0ab36cbf2426983e0ebdbd006
ssummerhays <spencersummerhays3@gmail.com>
2024-11-08 14:00:59 -0700

    Added an input in login for a name and fixed functioality for login/signup buttons

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 import './login.css'
 
 export function Login() {
@@ -8,14 +9,21 @@ export function Login() {
             <h1 className='h1-title'>GolfLeaderboard</h1>
             <form className='login-form' method="get" action="newTournament.html">
                 <div className="input-group mb-3">
+                    <input type="text" placeholder="Firstname Lastname" className="form-control"/>
+                </div>
+                <div className="input-group mb-3">
                     <input type="text" placeholder="email@example.com" className="form-control"/>
                 </div>
                 <div className="input-group mb-3">
                     <input type="password" placeholder="password" className="form-control"/>
                 </div>
                 <div>
-                    <button className="btn btn-primary" type="submit">Login</button>
-                    <button className="btn btn-secondary" type="submit">Sign Up</button>
+                    <Link to="newTournament">
+                        <button className="btn btn-primary" type="submit">Login</button>
+                    </Link>
+                    <Link to="newTournament">
+                        <button className="btn btn-secondary" type="submit">Sign Up</button>
+                    </Link>
                 </div>
             </form>
         </div> 
