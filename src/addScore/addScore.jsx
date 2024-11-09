12bc51595778ad5bc7c5072d55e74ca0faa6e97f
ssummerhays <spencersummerhays3@gmail.com>
2024-11-09 13:09:00 -0700

    Updated AddScore component to broadcast scores when the user adds a new score and an option to clear your total score to reset the round.

--- src/addScore/addScore.jsx
+++ src/addScore/addScore.jsx
@@ -1,30 +1,86 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { RecentScores } from './recentScores';
 import './addScore.css';
+import { Button, Modal } from 'react-bootstrap';
 
-export function AddScore() {
+export function AddScore(props) {
+  const [holeNumber, setHoleNumber] = React.useState(props.holeNumber);
+  const [scoreToPar, setScoreToPar] = React.useState(0);
+  const [showModal, setShowModal] = React.useState(false);
+
+  const navigate = useNavigate();
+
+  function addNewScore(holeNumber, scoreToPar) {
+    props.onAddNewScore(holeNumber, scoreToPar);
+    if (holeNumber !== 18) {
+      setHoleNumber(holeNumber + 1);
+    }
+  }
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
+
+  function clearTotalScore() {
+    props.onClearTotalScore();
+    setShowModal(false);
+    setHoleNumber(1);
+    setScoreToPar(0);
+  }
+
   return (
     <main className='addScore-main'>
-      <div>
+      <div className='add-new-scores'>
         <h2>Add a Score</h2>
-        <form method="get" action="leaderboard.html">
           <div>
-            <label for="hole-number">Hole#</label>
-            <input className="input-group mb-3" type="number" id="hole-number" min="1" max="18"/>
+            <label htmlFor="hole-number">Hole#</label>
+            <input 
+              className="input-group mb-3" 
+              type="number" id="hole-number" 
+              min="1" 
+              max="18" 
+              value={holeNumber} 
+              onChange={(e) => setHoleNumber(parseInt(e.target.value), 10)}
+            />
           </div>
           <div>
-            <label for="score">Score to Par</label>
-            <input type="number" id="score" className="input-group mb-3" min="-4" max="10"/>
+            <label htmlFor="score">Score to Par</label>
+            <input 
+              type="number" 
+              id="score" 
+              className="input-group mb-3" 
+              min="-4" max="10" 
+              value={scoreToPar} 
+              onChange={(e) => setScoreToPar(parseInt(e.target.value), 10)}
+            />
           </div>
-          <Link to="/leaderboard"><button className="btn btn-primary" type="submit">Submit</button></Link>
-          
-        </form>
+          <div className='buttons'>
+            <div>
+              <Button variant='primary' onClick={() => addNewScore(holeNumber, scoreToPar)} disabled={holeNumber < 1 || holeNumber > 18}>Submit</Button>
+            <Button variant='secondary' onClick={() => navigate('/leaderboard')}>Return</Button>
+            </div>
+            <Button variant='danger' onClick={handleOpenModal}>Clear Total Score</Button>
+          </div>
       </div>
       <div>
         <h2>Recent Scores</h2>
         <RecentScores />
       </div>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Clear Total Score</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to clear your total score?</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseModal}>Cancel</Button>
+          <Button variant='danger' onClick={clearTotalScore}>Clear Total Score</Button>
+        </Modal.Footer>
+      </Modal>
 
     </main>
   );
