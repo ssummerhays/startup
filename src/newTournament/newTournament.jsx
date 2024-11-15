63e53d61a6530d62ccb8b11f84e285c3541891de
ssummerhays <spencersummerhays3@gmail.com>
2024-11-15 14:13:34 -0700

    updated newTournament component to use service to create new tournaments and join existing tournaments

--- src/newTournament/newTournament.jsx
+++ src/newTournament/newTournament.jsx
@@ -1,50 +1,89 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import { MessageDialog } from '/src/login/messageDialog.jsx'
 import './newTournament.css'
 
 export function NewTournament(props) {
-  const [tournamentName, setTournamentName] = React.useState(props.tournamentName);
-  const [maxPlayers, setMaxPlayers] = React.useState(props.maxPlayers);
-  const [golfCourseName, setGolfCourseName] = React.useState(props.courseName);
+  const [createName, setCreateName] = React.useState("");
+  const [joinName, setJoinName] = React.useState(props.recentTournamentName);
+  const [maxPlayers, setMaxPlayers] = React.useState("");
+  const [golfCourseName, setGolfCourseName] = React.useState("");
+  const [displayError, setDisplayError] = React.useState(null);
 
   const navigate = useNavigate();
 
   async function createNewTournament() {
-    localStorage.setItem('tournamentName', tournamentName);
-    localStorage.setItem('maxPlayers', maxPlayers);
-    localStorage.setItem('courseName', golfCourseName);
-    props.onNewTournament(tournamentName, maxPlayers, golfCourseName);
-    navigate('/leaderboard')
+    const tournament = {
+      tournamentName: createName,
+      courseName: golfCourseName,
+      maxPlayers: maxPlayers,
+    }
+    const response = await fetch('/api/tournaments/create', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(tournament)
+    });
+
+    if (response?.status === 200) {
+      localStorage.setItem('tournamentName', createName);
+      navigate('/leaderboard');
+    } else {
+      const body = await response.json();
+      setDisplayError(`Error: ${body.msg}`);
+    }
+    
   }
   async function joinTournament() {
-    localStorage.setItem('tournamentName', tournamentName);
-    props.onNewTournament(tournamentName, maxPlayers, null);
-    navigate('/leaderboard')
+    const response = await fetch('/api/tournaments', {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' }
+    });
+
+    const body = await response.json();
+    
+    if (response?.status === 200) {
+      if (body[joinName]) {
+        localStorage.setItem('tournamentName', joinName);
+        navigate('/leaderboard');
+      } else {
+        setDisplayError(`Error: no tournament with the name ${joinName} exists`);
+      }
+    } else {
+      setDisplayError(`Error: ${body.msg}`);
+    }
   }
 
   return (
-    <main className='newTournament-main'>
-        <div className="new-tournament">
-            <h2>Create New Tournament</h2>
-            <div className="input-group mb-3">
-                <input type="text" placeholder="Tournament Name" className="form-control" value={tournamentName} onChange={(e) => setTournamentName(e.target.value)}/>
-            </div>
-            <div className="input-group mb-3">
-                <input type="number" placeholder="Max Players" className="form-control" min="1" value={maxPlayers} onChange={(e) => setMaxPlayers(e.target.value)}/>
-            </div>
-            <div className="input-group">
-                <input type="text" placeholder="Golf Course Name" className="form-control" value={golfCourseName} onChange={(e) => setGolfCourseName(e.target.value)}/>
-            </div>
-            <Button variant="primary" onClick={() => createNewTournament()} disabled={!tournamentName || !maxPlayers || !golfCourseName}>Create New Tournament</Button>
-        </div>
-        <div className="join-tournament">
-            <h2>Join Tournament</h2>
-            <div className="input-group">
-                <input className="form-control" type="text" placeholder="Tournament Name" value={tournamentName} onChange={(e) => setTournamentName(e.target.value)}/>
-            </div>
-            <Button variant='primary' onClick={() => joinTournament()} disabled={!tournamentName || !maxPlayers || !golfCourseName}>Join Tournament</Button>
-        </div>
-    </main>
+    <>
+      <main className='newTournament-main'>
+          <div className="new-tournament">
+              <h2>Create New Tournament</h2>
+              <div className="input-group mb-3">
+                  <input type="text" placeholder="Tournament Name" className="form-control" value={createName} 
+                  onChange={(e) => setCreateName(e.target.value)}/>
+              </div>
+              <div className="input-group mb-3">
+                  <input type="number" placeholder="Max Players" className="form-control" min="1" value={maxPlayers} 
+                  onChange={(e) => setMaxPlayers(e.target.value)}/>
+              </div>
+              <div className="input-group">
+                  <input type="text" placeholder="Golf Course Name" className="form-control" value={golfCourseName} 
+                  onChange={(e) => setGolfCourseName(e.target.value)}/>
+              </div>
+              <Button variant="primary" onClick={() => createNewTournament()} disabled={!createName || !maxPlayers || !golfCourseName}>Create New Tournament</Button>
+          </div>
+          <div className="join-tournament">
+              <h2>Join Tournament</h2>
+              <div className="input-group">
+                  <input className="form-control" type="text" placeholder="Tournament Name" value={joinName} 
+                  onChange={(e) => setJoinName(e.target.value)}/>
+              </div>
+              <Button variant='primary' onClick={() => joinTournament()} disabled={!joinName}>Join Tournament</Button>
+          </div>
+      </main>
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)}/>
+    </>
+    
   );
 }
