23c4e14c1009726554b4a7398b70acbf5537fbc5
ssummerhays <spencersummerhays3@gmail.com>
2024-11-15 12:20:11 -0700

    added endpoints for auth login, logout, and create and a users endpoint that gets the list of users

--- service/index.js
+++ service/index.js
@@ -14,6 +14,42 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.email];
+    if (user) {
+        res.status(409).send({msg: "existing user"});
+    } else {
+        const user = { email: req.body.email, name: req.body.name, password: req.body.password, recentScore: 0, totalScore: 0, currentHole: 1, token: uuid.v4() }
+        users[user.email] = user;
+        
+        res.send({ token: user.token });
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = users[req.body.email];
+  if (user) {
+    if (req.body.password === user.password) {
+      user.token = uuid.v4();
+      res.send({ token: user.token });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+        delete user.token;
+    }
+    res.status(204).end();
+})
+
+apiRouter.get('/users', (req, res) => {
+    res.send(users);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
