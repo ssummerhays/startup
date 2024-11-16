6dba4ecba7a1478fa41f3c30d838d385e2cc5ac1
ssummerhays <spencersummerhays3@gmail.com>
2024-11-16 11:54:22 -0700

    Updated addScore to work with backend service and because of the functionality of the service removed the clear total score button

--- src/addScore/addScore.jsx
+++ src/addScore/addScore.jsx
@@ -2,86 +2,98 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RecentScores } from './recentScores';
 import './addScore.css';
-import { Button, Modal } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { MessageDialog } from '../login/messageDialog';
 
 export function AddScore(props) {
-  const [holeNumber, setHoleNumber] = React.useState(props.holeNumber);
+  const [holeNumber, setHoleNumber] = React.useState(1);
   const [scoreToPar, setScoreToPar] = React.useState(0);
-  const [showModal, setShowModal] = React.useState(false);
+  const [displayError, setDisplayError] = React.useState(null);
+  const [userName, setUserName] = React.useState("");
 
   const navigate = useNavigate();
 
-  function addNewScore(holeNumber, scoreToPar) {
-    props.onAddNewScore(holeNumber, scoreToPar);
-    if (holeNumber !== 18) {
-      setHoleNumber(holeNumber + 1);
+  React.useEffect(() => {
+    async function getUserData() {
+      const response = await fetch('/api/users', {
+        method: 'GET',
+        headers: { 'content-type': 'application/json' }
+      });
+
+      const body = await response.json();
+      const user = body[props.email];
+      setUserName(user.userName);
+      setHoleNumber(parseInt(user.currentHole, 10) + 1);
+      setScoreToPar(user.totalScore);
+    }
+
+    getUserData();
+  }, [userName] );
+
+
+  async function addNewScore(holeNumber, scoreToPar) {
+    const body = {
+      email: props.email,
+      recentScore: scoreToPar,
+      hole: holeNumber
+    }
+    const response = await fetch('/api/tournaments/score', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body)
+    });
+    if (response.status === 200) {
+      props.onAddNewScore(holeNumber, scoreToPar);
+      if (holeNumber !== 18) {
+        setHoleNumber(holeNumber + 1);
+      }
+    } else {
+      const body = await response.json();
+      setDisplayError(`Error: ${body.msg}`);
     }
   }
 
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  }
-
-  function clearTotalScore() {
-    props.onClearTotalScore();
-    setShowModal(false);
-    setHoleNumber(1);
-    setScoreToPar(0);
-  }
-
   return (
-    <main className='addScore-main'>
-      <div className='add-new-scores'>
-        <h2>Add a Score</h2>
-          <div>
-            <label htmlFor="hole-number">Hole#</label>
-            <input 
-              className="input-group mb-3" 
-              type="number" id="hole-number" 
-              min="1" 
-              max="18" 
-              value={holeNumber} 
-              onChange={(e) => setHoleNumber(parseInt(e.target.value), 10)}
-            />
-          </div>
-          <div>
-            <label htmlFor="score">Score to Par</label>
-            <input 
-              type="number" 
-              id="score" 
-              className="input-group mb-3" 
-              min="-4" max="10" 
-              value={scoreToPar} 
-              onChange={(e) => setScoreToPar(parseInt(e.target.value), 10)}
-            />
-          </div>
-          <div className='buttons'>
+    <>
+      <main className='addScore-main'>
+        <div className='add-new-scores'>
+          <h2>Add a Score</h2>
             <div>
-              <Button variant='primary' onClick={() => addNewScore(holeNumber, scoreToPar)} disabled={holeNumber < 1 || holeNumber > 18}>Submit</Button>
-            <Button variant='secondary' onClick={() => navigate('/leaderboard')}>Return</Button>
+              <label htmlFor="hole-number">Hole#</label>
+              <input 
+                className="input-group mb-3" 
+                type="number" id="hole-number" 
+                min="1" 
+                max="18" 
+                value={holeNumber} 
+                onChange={(e) => setHoleNumber(parseInt(e.target.value), 10)}
+              />
             </div>
-            <Button variant='danger' onClick={handleOpenModal}>Clear Total Score</Button>
-          </div>
-      </div>
-      <div>
-        <h2>Recent Scores</h2>
-        <RecentScores />
-      </div>
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Clear Total Score</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Are you sure you want to clear your total score?</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleCloseModal}>Cancel</Button>
-          <Button variant='danger' onClick={clearTotalScore}>Clear Total Score</Button>
-        </Modal.Footer>
-      </Modal>
+            <div>
+              <label htmlFor="score">Score to Par</label>
+              <input 
+                type="number" 
+                id="score" 
+                className="input-group mb-3" 
+                min="-4" max="10" 
+                value={scoreToPar} 
+                onChange={(e) => setScoreToPar(parseInt(e.target.value), 10)}
+              />
+            </div>
+            <div className='buttons'>
+              <div>
+                <Button variant='primary' onClick={() => addNewScore(holeNumber, scoreToPar)} disabled={holeNumber < 1 || holeNumber > 18}>Submit</Button>
+              <Button variant='secondary' onClick={() => navigate('/leaderboard')}>Return</Button>
+              </div>
+            </div>
+        </div>
+        <div>
+          <h2>Recent Scores</h2>
+          <RecentScores />
+        </div>
 
-    </main>
+      </main>
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)}/>
+    </>
   );
 }
