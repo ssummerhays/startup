1396081e25d3f5adf94efbccf05b3fc9aa48c26b
ssummerhays <spencersummerhays3@gmail.com>
2024-12-07 17:01:01 -0700

    updated peerProxy to send the event to the original user as well as all others within the tournament

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -4,61 +4,54 @@ const uuid = require('uuid');
 function peerProxy(httpServer) {
     const wss = new WebSocketServer({noServer: true});
 
-    httpServer.on('upgrade', (request, socket, head) => {
-        wss.handleUpgrade(request, socket, head, function done(ws) {
-            wss.emit('connection', ws, request);
-        });
+  httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+  let connections = [];
+
+  wss.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws, tournamentName: null };
+    connections.push(connection);
+
+    ws.on('message', function message(data) {
+      const parsedData = JSON.parse(data);
+
+      if (parsedData.tournamentName) {
+        connection.tournamentName = parsedData.tournamentName;
+      }
+      connections.forEach((c) => {
+        if (c.tournamentName === connection.tournamentName) {
+          c.ws.send(data);
+        }
+      });
     });
 
-    let connections = [];
+    ws.on('close', () => {
+      const pos = connections.findIndex((o, i) => o.id === connection.id);
 
-    wss.on('connection', (ws) => {
-        const connection = { id: uuid.v4(), tournament: null, alive: true, ws: ws };
-
-        wss.on('message', (data) => {
-            try {
-                const message = JSON.parse(data);
-                if (message.tournament) {
-                    connection.tournament = message.tournament;
-                }
-            } catch (err) {
-                //Message does not contain tournament
-            }
-        });
-        
-        connections.push(connection);
-
-        wss.on('message', function message(data) {
-            connections.forEach((c) => {
-                if (c.tournament === connection.tournament && c.id !== connection.id) {
-                    c.ws.send(data);
-                }
-            });
-        });
-
-        wss.on('close', () => {
-            const pos = connections.findIndex((o, i) => o.id === connection.id);
-
-            if (pos >= 0) {
-                connections.splice(pos, 1);
-            }
-        });
-
-        ws.on('pong', () => {
-            connection.alive = true;
-        });
+      if (pos >= 0) {
+        connections.splice(pos, 1);
+      }
     });
 
-    setInterval(() => {
-        connections.forEach((c) => {
-            if (!c.alive) {
-                c.ws.terminate();
-            } else {
-                c.alive = false;
-                c.ws.ping();
-            }
-        });
-    }, 10000);
+    ws.on('pong', () => {
+      connection.alive = true;
+    });
+  });
+
+  setInterval(() => {
+    connections.forEach((c) => {
+      if (!c.alive) {
+        c.ws.terminate();
+      } else {
+        c.alive = false;
+        c.ws.ping();
+      }
+    });
+  }, 10000);
 }
 
 module.exports = { peerProxy };
